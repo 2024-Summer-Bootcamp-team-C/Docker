cb7ed9b9a2a310797a591d5bd67a025bfcce9ebe
박근채 <pgc0419@gmail.com>
2024-07-29 21:17:00 +0900

    Fix: 도메인 변경

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 compose_file="docker-compose.yml"
 
-domains=(weinhistory.com)
+domains=(weinjeon.com)
 rsa_key_size=4096
 data_path="./certbot"
 email="pgc0419@gmail.com" # Adding a valid address is strongly recommended
